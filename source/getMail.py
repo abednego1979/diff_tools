8cbd8217014d28244c438b7d4785fc2b987e6a40
abednego1979 <abednego1979@163.com>
2018-04-20 17:15:15 +0800

    patch

--- source/getMail.py
+++ source/getMail.py
@@ -86,7 +86,7 @@ username = "abednego1979@163.com"
 pp = poplib.POP3(host)
 pp.set_debuglevel(1)
 pp.user(username)
-password=raw_input('mail box password('+username+'):')
+password=input('mail box password('+username+'):')
 pp.pass_(password)
 
 ret = pp.stat()
@@ -121,7 +121,7 @@ print ('--------')
 print (ret)
 
 while True:
-    a=raw_input('which to get?'+'('+'/'.join(ret.keys())+')')
+    a=input('which to get?'+'('+'/'.join(ret.keys())+')')
     if a in ret.keys():
         break
 
